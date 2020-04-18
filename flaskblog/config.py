0bae29590f5e6d401da3c1df0c5bb5e2ad321ff8
Abir Bhowmick <abir1907@gmail.com>
2020-04-18 11:07:07 +0530

    Flask-Blog project

---
+++ flaskblog/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Config:
+    SECRET_KEY = 'd301ac3a10e2e161100a9232d5c6036e'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    # MAIL_USERNAME = 'abir1907@gmail.com'
+    # MAIL_PASSWORD = 'rinapinku'
